e89a5bcc5a03315dee04af5e3037834b47173269
tricko93 <tricko93@outlook.com>
2022-07-26 10:24:33 +0200

    Added insert() function in the utility.c. Added struct declaration in utility.h.

--- utility.c
+++ utility.c
@@ -1,1 +1,19 @@
 #include "utility.h"
+
+Node* insert(Node* head, char data[100])
+{
+        Node *temp, *p;
+        temp = (Node*)malloc(sizeof(Node));
+        strncpy(temp->data, data, strlen(data));
+        temp->next = NULL;
+        if(head==NULL)
+        {
+                head = temp;
+        }
+        else
+        {
+                for(p=head; p->next!=NULL; p=p->next);
+                p->next = temp;
+        }
+        return temp;
+}
--- utility.h
+++ utility.h
@@ -1,6 +1,17 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include <stdlib.h>
+#include <string.h>
 
+// Declaration for the node.
+typedef struct node {
+        char data[100];
+        struct node *prev;
+        struct node *next;
+} Node;
+
+// Forward definitions for functions.
+Node* insert(Node* head, char data[100]);
 
 #endif // UTILITY_H
