32d502af25764d57c70f65f8bcd99f0c769462d5
tricko93 <tricko93@outlook.com>
2022-07-26 18:21:25 +0200

    Added convertToVariable(head, str) function in utility.h and utility.c.
    Functions concates all the list elements into one string variable (hence
    the name). Useful function when the whole path is needed to be used as
    parameter to some other function.

--- utility.c
+++ utility.c
@@ -1,19 +1,34 @@
 #include "utility.h"
 
-Node* insert(Node* head, char data[100])
+Node *insert(Node *head, char data[100])
 {
         Node *temp, *p;
-        temp = (Node*)malloc(sizeof(Node));
-        strncpy(temp->data, data, strlen(data));
+        temp = (Node *)malloc(sizeof(Node));
+        strncpy(temp->data, data, 100);
         temp->next = NULL;
-        if(head==NULL)
+        if (head == NULL)
         {
                 head = temp;
         }
         else
         {
-                for(p=head; p->next!=NULL; p=p->next);
+                for (p = head; p->next != NULL; p = p->next)
+                        ;
                 p->next = temp;
         }
         return temp;
+}
+
+void convertToVariable(Node *head, char *variable)
+{
+        int offset = 0, sum = 0;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+                offset = strlen(temp->data);
+                strncpy(variable + sum, temp->data, offset);
+                temp = temp->next;
+                sum += offset;
+        }
+        variable[sum] = '\0';
 }
--- utility.h
+++ utility.h
@@ -5,13 +5,15 @@
 #include <string.h>
 
 // Declaration for the node.
-typedef struct node {
+typedef struct node
+{
         char data[100];
         struct node *prev;
         struct node *next;
 } Node;
 
 // Forward definitions for functions.
-Node* insert(Node* head, char data[100]);
+Node *insert(Node *head, char data[100]);
+void convertToVariable(Node *head, char *variable);
 
 #endif // UTILITY_H
