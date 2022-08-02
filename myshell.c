e5583cf0733f25edcc267f34c106381f848c6cf4
tricko93 <tricko93@outlook.com>
2022-08-02 17:24:34 +0200

    Added prompt and RunProgram function.

--- myshell.c
+++ myshell.c
@@ -1,5 +1,22 @@
 #include "myshell.h"
- 
+
+void EchoInternalCmd(char *line)
+{
+  char *m = Split(line, ' ');
+  printf("%s\n", ++m);
+}
+
+void RunProgram(char * const prog)
+{
+  char s[100];
+  getcwd(s, 100);
+  int s_sz = strlen(s);
+  s[s_sz]='\\';
+  strncpy(s+s_sz+1, prog, strlen(prog));
+  s[s_sz+1+strlen(prog)]='\0';
+  ExecuteProgram(s);
+}
+
 int main(int argc, char const *argv[], char const *envp[])
 {
   // Declare variables
@@ -19,26 +36,15 @@ int main(int argc, char const *argv[], char const *envp[])
 
   while (1)
   {
+    char p[100];
+    printf("%s>", getcwd(p,100));
+
     fgets(line, MAX_SIZE, input);
     strtok(line, "\n");
 
     if (Position1(line, "cd")==0)
     {
-      int line_sz = strlen(line);
-      if(line_sz == 2)
-      {
-        char s[100];
-        printf("%s\n", getcwd(s, 100));
-      }
-      else
-      {
-        char *r = Split(line, ' ');
-        ++r;
-          if (chdir(r))
-          {
-            fprintf(stdout, "Specified path cannot be found.\n");
-          }
-      }
+      CdInternalCmd(line);
     }
     else if(Position1(line, "clr")==0)
       system("cls");
@@ -47,8 +53,19 @@ int main(int argc, char const *argv[], char const *envp[])
       char s[100];
       DirInternalCmd(getcwd(s, 100));
     }
+    else if(Position1(line, "environ")==0)
+    {
+      for (int i = 0; envp[i] != NULL; i++)
+        printf("%s\n", envp[i]);
+    }
+    else if(Position1(line, "echo")==0)
+    {
+      EchoInternalCmd(line);
+    }
     else if(Position1(line, "quit")==0)
       exit(0);
+    else
+      RunProgram(line);
   }
 
   if(input != stdin)
