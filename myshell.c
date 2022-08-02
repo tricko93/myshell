8828ea1540d044944f9ef1d1cabef187197c3989
tricko93 <tricko93@outlook.com>
2022-08-02 14:31:13 +0200

    Added input to the shell.

--- myshell.c
+++ myshell.c
@@ -1,7 +1,33 @@
 #include "myshell.h"
-
+ 
 int main(int argc, char const *argv[], char const *envp[])
 {
-  /* code */
+  // Declare variables
+  Node *head = Insert(NULL, "C:\\");
+  char line[MAX_SIZE], path[MAX_SIZE];
+  FILE *input = stdin;
+
+  if (argc == 2) // Batch file is given
+  {
+    input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+      fprintf(stdout, "Input file was not found.\n");
+      exit(1);
+    }
+  }
+
+  while (1)
+  {
+    fgets(line, MAX_SIZE, input);
+    strtok(line, "\n");
+
+    if(Position1(line, "quit")==0)
+      exit(0);
+  }
+
+  if(input != stdin)
+    fclose(input);
+
   return 0;
 }
--- myshell.h
+++ myshell.h
@@ -1,4 +1,10 @@
 #ifndef MYSHELL_H
 #define MYSHELL_H
 
+#include "utility.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 250
+
 #endif // MYSHELL_H
